bdabb275c9cdee8f4cebdf0eec70abc328062fcf
Alexis Burgos <alexis.burgos@showroomprive.net>
2018-05-30 14:05:42 +0200

    Trying to remove ignored files

---
+++ core/model/BaseModel.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/*
+*  Generic abstract class
+*  Give base methods/attributs common to all model
+*  All model must override this class
+*/
+class BaseModel {
+    constructor(id) {
+        this.id = id;
+    }
+}
+exports.BaseModel = BaseModel;
+//# sourceMappingURL=BaseModel.js.map
