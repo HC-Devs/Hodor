d2a4af14df50189e2a41937f7cb6415d512884ba
Alexis Burgos <alexis.burgos@showroomprive.net>
2018-05-30 14:17:50 +0200

    [+] Add Ship model

---
+++ core/model/Ship.ts
@@ -0,0 +1,20 @@
+import {BaseModel} from "./BaseModel";
+
+export class Ship extends BaseModel {
+
+    name: string;
+    type: number;
+    fkUserId: string;
+
+    constructor(id: string, name: string, type: number, fkUserId: string) {
+        super(id);
+
+        this.name = name;
+        this.type = type;
+        this.fkUserId = fkUserId;
+    }
+
+    getArray(): Array<string> {
+        return Array<string>(this.id, this.name, String(this.type), this.fkUserId);
+    }
+}
