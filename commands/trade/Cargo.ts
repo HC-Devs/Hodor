f47bce2b4ae7229c537ad91e347dcf872e6314d3
Alexis Burgos <alexis.burgos@showroomprive.net>
2018-06-07 19:20:21 +0200

    [+] Add all trade commands

---
+++ commands/trade/Cargo.ts
@@ -0,0 +1,21 @@
+import {BotTS} from "../../BotTS";
+import {BaseModuleCommand} from "../BaseModuleCommand";
+import {Message, Snowflake} from "discord.js";
+
+const allowedUsers = [];
+const allowedRoles = [];
+const allowedChannels = ["421655362966650880"];
+const allowedGuilds = [];
+
+export class Cargo extends BaseModuleCommand {
+
+    constructor(bot: BotTS) {
+        super(bot, "cargo");
+    }
+
+    async runCommand(message: Message, level: number, user: Snowflake) {
+        //TODO 
+    }
+}
+
+module.exports = Cargo;
